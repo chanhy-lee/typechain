1d01a659ffa4c40e9fd4e75c19bdf8cdea7f5d62
Chan-hyeong Lee <dannychh@khu.ac.kr>
2020-10-15 02:39:20 +0900

    Update typechain

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+class Block { // Create 'Block' structure.
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+
+const genesisBlock: Block = new Block(0, "2020", "", "Hello", 123); // Dummy block for test.
+
+let blockchain: [Block] = [genesisBlock];
+
+console.log(blockchain);
+
+export{};
